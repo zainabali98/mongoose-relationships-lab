c7423724b9eb8f92d5a107233410693926ffffe3
zainab <zainab_ali_juma@hotmail.com>
2026-09-13 12:39:59 +0300

    first commit: building schema

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+})
---
+++ server.js
@@ -0,0 +1,81 @@
+// imports
+const express = require("express") //importing express package
+const app = express() // creates a express application
+const dotenv = require("dotenv").config() //this allows me to use my .env values in this file
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Middleware
+app.use(express.static('public')); //all static files are in the public folder
+app.use(express.urlencoded({ extended: false })); // this will allow us to see the data being sent in the POST or PUT
+app.use(methodOverride("_method")); // Changes the method based on the ?_method
+app.use(morgan("dev")) // logs the requests as they are sent to our sever in the terminal
+
+
+
+async function conntectToDB(){ //connection to the database
+    try{
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Connected to Database")
+    }
+    catch(error){
+        console.log("Error Occured",error)
+    }
+}
+
+
+conntectToDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Routes go here
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Listening on port " + 3000)
+}) // Listen on port 3000
